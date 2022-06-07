fab737409d66cbf9a0766bdb7196344ddec932a1
George Jieh <george.jieh@gmail.com>
2022-06-07 00:23:36 -0700

    redact file paths in codes

--- R-analysis/bellabeat-analysis-r-code.R
+++ R-analysis/bellabeat-analysis-r-code.R
@@ -3,8 +3,8 @@ install.packages('tidyverse')
 library(tidyverse)
 
 # import data
-setwd("/Users/nancy/Documents/Git Repo/Google Case Studies/bellabeat-analysis/R-analysis/csv-for-import")
-getwd() 
+setwd("/.../bellabeat-analysis/R-analysis/csv-for-import")
+getwd()
 daily_sleep <- read.csv("daily_sleep_data.csv", stringsAsFactors = T)
 weight_log <- read.csv("weight_log.csv", stringsAsFactors = T)
 daily_activity <- read.csv("daily_activity.csv", stringsAsFactors = T)
@@ -18,16 +18,16 @@ str(daily_activity)
 str(weight_log)
 
 # set Id as factor
-daily_sleep$Id <- as.factor(daily_sleep$Id) 
+daily_sleep$Id <- as.factor(daily_sleep$Id)
 daily_activity$Id <- as.factor(daily_activity$Id)
 weight_log$Id <- as.factor(weight_log$Id)
 
 # calculate height of participants and set BMI standards
 weight_log$Height <- sqrt(weight_log$WeightKg/weight_log$BMI)*100
-weight_log$ObesityLevel <- ifelse(weight_log$BMI < 18, 'UnderWeight', 
+weight_log$ObesityLevel <- ifelse(weight_log$BMI < 18, 'UnderWeight',
                                   ifelse(weight_log$BMI >= 18 & weight_log$BMI < 25,
-                                         'HealthyWeight', 
-                                         ifelse(weight_log$BMI >= 25 & weight_log$BMI < 30, 
+                                         'HealthyWeight',
+                                         ifelse(weight_log$BMI >= 25 & weight_log$BMI < 30,
                                                 'OverWeight',
                                                 ifelse(weight_log$BMI >= 30 & weight_log$BMI < 40,
                                                        'Obese', 'Severely Obese'))))
@@ -46,18 +46,18 @@ nrow(daily_sleep)
 nrow(weight_log)
 
 # quick summary statistics
-daily_activity %>% 
+daily_activity %>%
   select(TotalSteps, TotalDistance, TrackerDistance, LoggedActivitiesDistance,
          VeryActiveDistance, ModeratelyActiveDistance, LightActiveDistance,
          SedentaryActiveDistance, VeryActiveMinutes, FairlyActiveMinutes,
          LightlyActiveMinutes, SedentaryMinutes, Calories) %>%
   summary()
 
-daily_sleep %>% 
-  select(TotalMinutesAsleep, TotalTimeInBed) %>% 
+daily_sleep %>%
+  select(TotalMinutesAsleep, TotalTimeInBed) %>%
   summary()
 
-weight_log %>% 
+weight_log %>%
   select(WeightKg, WeightPounds, BMI, Height, IsManualReport) %>%
 summary()
 
@@ -65,7 +65,7 @@ summary()
 ggplot(data=daily_activity, aes(x=TotalSteps, y=SedentaryMinutes)) +
   geom_point() + geom_smooth()
 
-ggplot(data=daily_sleep, aes(x=TotalMinutesAsleep, y=TotalTimeInBed)) + 
+ggplot(data=daily_sleep, aes(x=TotalMinutesAsleep, y=TotalTimeInBed)) +
   geom_point() + geom_smooth()
 
 ggplot(data=weight_log, aes(x=Height, fill=Id)) +
@@ -83,34 +83,34 @@ str(combined_data2)
 str(combined_data1)
 
 # exploring data of combined datasets
-ggplot(data=combined_data2, aes(x=SedentaryMinutes, y=BMI, 
+ggplot(data=combined_data2, aes(x=SedentaryMinutes, y=BMI,
                                 fill=ObesityLevel)) + geom_boxplot() +
   geom_jitter()
 
-ggplot(data=combined_data2, aes(x=SedentaryMinutes, y=Height, 
+ggplot(data=combined_data2, aes(x=SedentaryMinutes, y=Height,
                                 colour=ObesityLevel)) + geom_point()
 
-ggplot(data=combined_data2, aes(x=SedentaryMinutes, y=WeightKg, 
+ggplot(data=combined_data2, aes(x=SedentaryMinutes, y=WeightKg,
                                 colour=ObesityLevel)) + geom_point()
 
-ggplot(data=combined_data2, aes(x=SedentaryMinutes, y=TotalSteps, 
-                                colour=ObesityLevel)) + geom_point() + 
+ggplot(data=combined_data2, aes(x=SedentaryMinutes, y=TotalSteps,
+                                colour=ObesityLevel)) + geom_point() +
   geom_smooth() + ylim(c(0,20500))
 
-ggplot(data=combined_data1, aes(x=TotalTimeInBed, y=SedentaryMinutes, 
+ggplot(data=combined_data1, aes(x=TotalTimeInBed, y=SedentaryMinutes,
                                 )) + geom_point()
 
-ggplot(data=combined_data2, aes(x=TotalTimeInBed, y=TotalSteps, 
+ggplot(data=combined_data2, aes(x=TotalTimeInBed, y=TotalSteps,
                                 colour=ObesityLevel)) + geom_point()
 
 plot_data <- combined_data2 %>% filter(TotalTimeInBed != SedentaryMinutes)
-ggplot(data=plot_data, aes(x=ObesityLevel, y=SedentaryMinutes, 
+ggplot(data=plot_data, aes(x=ObesityLevel, y=SedentaryMinutes,
                                 fill=ObesityLevel)) + geom_boxplot()
 
-ggplot(data=combined_data2, aes(x=ObesityLevel, y=TotalSteps, 
+ggplot(data=combined_data2, aes(x=ObesityLevel, y=TotalSteps,
                            fill=Id)) + geom_boxplot()
 
-ggplot(data=combined_data1, aes(x=TotalTimeInBed, y=TotalSteps, 
+ggplot(data=combined_data1, aes(x=TotalTimeInBed, y=TotalSteps,
                                 )) + geom_point() + geom_smooth()
 
 ggplot(data=combined_data2, aes(y=Calories, x=TotalSteps, colour=ObesityLevel)) +
@@ -125,25 +125,25 @@ ggplot(data=combined_data1, aes(y=Calories, x=VeryActiveMinutes, colour=Calories
 ggplot(data=combined_data1, aes(x=TotalMinutesAsleep, y=SedentaryMinutes)) +
   geom_point() + geom_smooth()
 
-# most things drawn with combined_data2 does not produce useful results due to 
+# most things drawn with combined_data2 does not produce useful results due to
 # small unique Id sample size.
 
-combined_data1$NonSleepingSedentaryMinutes <- combined_data1$SedentaryMinutes - 
+combined_data1$NonSleepingSedentaryMinutes <- combined_data1$SedentaryMinutes -
   combined_data1$TotalTimeInBed
-ggplot(data=combined_data1, aes(y=Calories, x=NonSleepingSedentaryMinutes, colour=Calories)) + 
+ggplot(data=combined_data1, aes(y=Calories, x=NonSleepingSedentaryMinutes, colour=Calories)) +
   geom_point() + geom_smooth()
 
-combined_data1$ActiveMinutes <- combined_data1$VeryActiveMinutes + 
+combined_data1$ActiveMinutes <- combined_data1$VeryActiveMinutes +
   combined_data1$FairlyActiveMinutes + combined_data1$LightlyActiveMinutes
 
-combined_data2$ActiveMinutes <- combined_data2$VeryActiveMinutes + 
+combined_data2$ActiveMinutes <- combined_data2$VeryActiveMinutes +
   combined_data2$FairlyActiveMinutes + combined_data2$LightlyActiveMinutes
 
-ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=Calories)) + 
+ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=Calories)) +
   geom_point() + geom_smooth()
-ggplot(data=combined_data1, aes(y=TotalSteps, x=ActiveMinutes, colour=Calories)) + 
+ggplot(data=combined_data1, aes(y=TotalSteps, x=ActiveMinutes, colour=Calories)) +
   geom_point() + geom_smooth()
-ggplot(data=combined_data1, aes(y=TotalMinutesAsleep, x=ActiveMinutes, colour=Calories)) + 
+ggplot(data=combined_data1, aes(y=TotalMinutesAsleep, x=ActiveMinutes, colour=Calories)) +
   geom_point() + geom_smooth()
 
 ggplot(data=combined_data2, aes(x=ActiveMinutes)) + geom_density(aes(fill=ObesityLevel))
@@ -154,9 +154,9 @@ ggplot(data=combined_data2, aes(x=LightlyActiveMinutes)) + geom_density(aes(fill
 
 #meaningful charts and graphs
 
-ggplot(data=combined_data1, aes(y=TotalSteps, x=ActiveMinutes, colour=Calories)) + 
+ggplot(data=combined_data1, aes(y=TotalSteps, x=ActiveMinutes, colour=Calories)) +
   geom_point() + xlim(c(1,560)) +
-  xlab("Minutes Active") + ylab("Number of Steps") + 
+  xlab("Minutes Active") + ylab("Number of Steps") +
   labs(colour="Calories Burned") +
   ggtitle("Daily Calories Burned Based on Length of Activity and Total Steps") +
   theme(axis.title.x = element_text(size=12),
@@ -167,9 +167,9 @@ ggplot(data=combined_data1, aes(y=TotalSteps, x=ActiveMinutes, colour=Calories))
         legend.text = element_text(size=10),
         plot.title = element_text(size=18, hjust = 0.5))
 
-ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=Calories)) + 
+ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=Calories)) +
   geom_point() + geom_smooth(fill=NA) + xlim(c(1,560)) +
-  xlab("Minutes Active") + 
+  xlab("Minutes Active") +
   ylab("Calories Burned") + labs(colour="Calories Burned") +
   ggtitle("Daily Calories Burned Based on Minutes Active") +
   theme(axis.title.x = element_text(size=12),
@@ -183,13 +183,13 @@ ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=Calories)) +
 combined_data1$PercentageofActiveTimeOnLightActivity <- (combined_data1$LightlyActiveMinutes /
                                                            combined_data1$ActiveMinutes) * 100
 
-combined_data1$PercentageofActiveTimeOnHigherActivity <- ((combined_data1$FairlyActiveMinutes + 
+combined_data1$PercentageofActiveTimeOnHigherActivity <- ((combined_data1$FairlyActiveMinutes +
                                                              combined_data1$VeryActiveMinutes)/
                                                            combined_data1$ActiveMinutes) * 100
 
-ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=PercentageofActiveTimeOnLightActivity)) + 
+ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=PercentageofActiveTimeOnLightActivity)) +
   geom_point() + geom_smooth(fill=NA) + xlim(c(1,560)) +
-  xlab("Minutes Active") + 
+  xlab("Minutes Active") +
   ylab("Calories Burned") + labs(colour="% of Active Time Lightly Active") +
   ggtitle("Daily Calories Burned Based on Minutes Active (Light Activity)") +
   theme(axis.title.x = element_text(size=12),
@@ -200,9 +200,9 @@ ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=Percentageof
         legend.text = element_text(size=10),
         plot.title = element_text(size=18, hjust = 0.5))
 
-ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=PercentageofActiveTimeOnHigherActivity)) + 
+ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=PercentageofActiveTimeOnHigherActivity)) +
   geom_point() + geom_smooth(fill=NA) + xlim(c(1,560)) +
-  xlab("Minutes Active") + 
+  xlab("Minutes Active") +
   ylab("Calories Burned") + labs(colour="% of Active Time Exerising") +
   ggtitle("Daily Calories Burned Based on Minutes Active (Exercise)") +
   theme(axis.title.x = element_text(size=12),
@@ -213,9 +213,9 @@ ggplot(data=combined_data1, aes(y=Calories, x=ActiveMinutes, colour=Percentageof
         legend.text = element_text(size=10),
         plot.title = element_text(size=18, hjust = 0.5))
 
-ggplot(data=combined_data1, aes(y=Calories, x=PercentageofActiveTimeOnHigherActivity, colour=Calories)) + 
+ggplot(data=combined_data1, aes(y=Calories, x=PercentageofActiveTimeOnHigherActivity, colour=Calories)) +
   geom_point() + geom_smooth(fill=NA) + xlim(c(1,100)) +
-  xlab("% of Active Time Exercising") + 
+  xlab("% of Active Time Exercising") +
   ylab("Calories Burned") + labs(colour="Calories Burned") +
   ggtitle("Daily Calories Burned Based on % of Active Time Spent Exercising") +
   theme(axis.title.x = element_text(size=12),
@@ -226,9 +226,9 @@ ggplot(data=combined_data1, aes(y=Calories, x=PercentageofActiveTimeOnHigherActi
         legend.text = element_text(size=10),
         plot.title = element_text(size=18, hjust = 0.5))
 
-ggplot(data=combined_data1, aes(y=TotalSteps, x=NonSleepingSedentaryMinutes, colour=Calories)) + 
+ggplot(data=combined_data1, aes(y=TotalSteps, x=NonSleepingSedentaryMinutes, colour=Calories)) +
   geom_point() + xlim(c(0,1000)) + ylim(c(0,25000)) + geom_smooth(fill=NA) +
-  xlab("Minutes Sedentary While Not In Bed") + 
+  xlab("Minutes Sedentary While Not In Bed") +
   ylab("Total Steps Taken") + labs(colour="Calories Burned") +
   ggtitle("Daily Steps Taken In Relation to Total Time Sedentary While Not In Bed") +
   theme(axis.title.x = element_text(size=12),
@@ -241,7 +241,7 @@ ggplot(data=combined_data1, aes(y=TotalSteps, x=NonSleepingSedentaryMinutes, col
 
 ggplot(data=combined_data2, aes(y=Calories, x=TotalSteps, colour=ObesityLevel)) +
   geom_point() + geom_smooth(fill=NA) +
-  xlab("Total Steps Taken") + 
+  xlab("Total Steps Taken") +
   ylab("Calories Burned") + labs(colour="Obesity Level") +
   ggtitle("Daily Calories Burned Based on Total Steps Taken (Obesity Level)") +
   theme(axis.title.x = element_text(size=12),
@@ -252,9 +252,9 @@ ggplot(data=combined_data2, aes(y=Calories, x=TotalSteps, colour=ObesityLevel)) 
         legend.text = element_text(size=10),
         plot.title = element_text(size=18, hjust = 0.5))
 
-ggplot(data=combined_data2, aes(x=ActiveMinutes)) + 
+ggplot(data=combined_data2, aes(x=ActiveMinutes)) +
   geom_density(aes(fill=ObesityLevel), alpha=0.5) +
-  xlab("Minutes Active") + 
+  xlab("Minutes Active") +
   ylab("Density") + labs(fill="Obesity Level") +
   ggtitle("Daily Active Minutes Density Chart") +
   theme(axis.title.x = element_text(size=12),
@@ -265,9 +265,9 @@ ggplot(data=combined_data2, aes(x=ActiveMinutes)) +
         legend.text = element_text(size=10),
         plot.title = element_text(size=18, hjust = 0.5))
 
-ggplot(data=combined_data2, aes(x=Calories)) + 
+ggplot(data=combined_data2, aes(x=Calories)) +
   geom_density(aes(fill=ObesityLevel), alpha=0.5) +
-  xlab("Calories Burned") + 
+  xlab("Calories Burned") +
   ylab("Density") + labs(fill="Obesity Level") +
   ggtitle("Daily Calories Burned Density Chart") +
   theme(axis.title.x = element_text(size=12),
@@ -278,12 +278,12 @@ ggplot(data=combined_data2, aes(x=Calories)) +
         legend.text = element_text(size=10),
         plot.title = element_text(size=18, hjust = 0.5))
 
-combined_data2$ExerciseMinutes <- combined_data2$VeryActiveMinutes + 
+combined_data2$ExerciseMinutes <- combined_data2$VeryActiveMinutes +
   combined_data2$FairlyActiveMinutes
 
-ggplot(data=combined_data2, aes(x=ExerciseMinutes)) + 
+ggplot(data=combined_data2, aes(x=ExerciseMinutes)) +
   geom_density(aes(fill=ObesityLevel), alpha=0.5) +
-  xlab("Minutes Exercising") + 
+  xlab("Minutes Exercising") +
   ylab("Density") + labs(fill="Obesity Level") +
   ggtitle("Daily Minutes Exercising Density Chart") +
   theme(axis.title.x = element_text(size=12),
@@ -294,9 +294,9 @@ ggplot(data=combined_data2, aes(x=ExerciseMinutes)) +
         legend.text = element_text(size=10),
         plot.title = element_text(size=18, hjust = 0.5))
 
-ggplot(data=combined_data2, aes(x=LightlyActiveMinutes)) + 
+ggplot(data=combined_data2, aes(x=LightlyActiveMinutes)) +
   geom_density(aes(fill=ObesityLevel), alpha = 0.5) +
-  xlab("Lightly Active Minutes") + 
+  xlab("Lightly Active Minutes") +
   ylab("Density") + labs(fill="Obesity Level") +
   ggtitle("Daily Lightly Active Minutes Density Chart") +
   theme(axis.title.x = element_text(size=12),
